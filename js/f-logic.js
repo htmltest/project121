2c74aac93f907043ef01c6eefc276b547532c78f
htmltest <struev.vs@gmail.com>
2019-10-25 16:36:10 +0300

    fix order steps gtags

--- js/f-logic.js
+++ js/f-logic.js
@@ -1587,13 +1587,16 @@ $(document).ready(function() {
             var productID = $(this).attr('data-product');
             var formName = $(this).attr('data-name');
             var productPrice = $(this).attr('data-price');
+            if (typeof (productPrice) == 'undefined') {
+                productPrice = '';
+            }
             var step = $(this).attr('data-step');
             var category = $(this).attr('data-category');
             var coupon = $('#order-promo').val();
             if (typeof (coupon) == 'undefined') {
                 coupon = '';
             }
-            if (typeof (productID) != 'undefined' && typeof (formName) != 'undefined' && typeof (step) != 'undefined' && typeof (category) != 'undefined' && typeof (productPrice) != 'undefined') {
+            if (typeof (productID) != 'undefined' && typeof (formName) != 'undefined' && typeof (step) != 'undefined' && typeof (category) != 'undefined') {
                 var data = {
                     'checkout_step': step,
                     'value': productPrice,
@@ -1786,40 +1789,6 @@ function updatePrecalc(curForm, statusChange) {
                                 ]
                             };
                             gtag('event', 'product_select_program', dataTag);
-                        }
-
-                        if (typeof (curForm.data('firstPrecalc')) == 'undefined') {
-                            var productPrice = data.response.SUM;
-                            var step = curForm.attr('data-step');
-                            var category = curForm.attr('data-category');
-                            var coupon = $('#order-promo').val();
-                            if (typeof (coupon) == 'undefined') {
-                                coupon = '';
-                            }
-                            if (typeof (productID) != 'undefined' && typeof (formName) != 'undefined' && typeof (step) != 'undefined' && typeof (category) != 'undefined' && typeof (productPrice) != 'undefined') {
-                                var dataTag = {
-                                    'checkout_step': step,
-                                    'value': productPrice,
-                                    'currency': 'RUB',
-                                    "items": [
-                                                {
-                                                    'id': productID,
-                                                    'name': formName,
-                                                    'list_name': 'Страница_заявки',
-                                                    'brand': 'СМП-Страхование',
-                                                    'category': category,
-                                                    'list_position': 1,
-                                                    'quantity': 1,
-                                                    'price': productPrice
-                                                }
-                                    ],
-                                    'coupon': coupon
-                                };
-                                if (step == 1) {
-                                    gtag('event', 'begin_checkout', dataTag);
-                                    curForm.data('firstPrecalc', true);
-                                }
-                            }
                         }
                     }
                 } else {
